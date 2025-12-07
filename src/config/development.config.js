e9dcb4e9be90b5ce03ad02a1797e9cb478d1d138
Renaldo Louis <renaldo.louis@commsult.id>
2025-12-07 15:11:36 +0700

    update production api link to subdomain

--- src/config/development.config.js
+++ src/config/development.config.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   api: {
-    baseURL: 'https://daycipta-tech.com/api',
+    baseURL: 'https://app.siriusbi.com/api',
+    // baseURL: 'http://localhost:3000',
   },
   client: {
-    baseURL: 'https://daycipta-tech.com/api',
+    baseURL: 'https://app.siriusbi.com/api',
+    // baseURL: 'http://localhost:3000',
   },
 };
--- src/config/production.config.js
+++ src/config/production.config.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   api: {
-    baseURL: 'https://daycipta-tech.com/api',
+    baseURL: 'https://app.siriusbi.com/api',
   },
   client: {
-    baseURL: 'https://daycipta-tech.com/api',
+    baseURL: 'https://app.siriusbi.com/api',
   },
 };
